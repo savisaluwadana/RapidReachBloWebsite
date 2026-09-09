0c5551197230124ef97d6d01603d10be937416a6
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-09 05:51:19 +0530

    Load category editor styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./directory.css";
+import "./cms-category.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 
